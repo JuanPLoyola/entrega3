8f8d36a521e49451a2a5578de6e2fc2c813eb791
Juan Pablo <a20180951@pucp.edu.pe>
2024-09-19 02:34:11 -0500

    v4

--- app/src/main/java/com/example/proyecto/producto.java
+++ app/src/main/java/com/example/proyecto/producto.java
@@ -1,6 +1,8 @@
 package com.example.proyecto;
 
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,12 @@ public class producto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
+
+        Button button_add = findViewById(R.id.button_add);
+        // Redireccionar al carrito
+        button_add.setOnClickListener(view -> {
+            Intent intent = new Intent(producto.this, CartActivity.class);
+            startActivity(intent);
+        });
     }
 }
